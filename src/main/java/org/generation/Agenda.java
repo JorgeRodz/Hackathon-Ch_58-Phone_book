b503507e74d93875aed53476c2b1375b5c593873
PedroSV22 <pedroserrano2304@gmail.com>
2025-09-29 17:54:02 -0600

    add: the agenda section is added

--- src/main/java/org/generation/Agenda.java
+++ src/main/java/org/generation/Agenda.java
@@ -17,7 +17,7 @@ public class Agenda {
 	}
 
 	public void setMaximunCapacity(int maximumCapacity) {
-		this.maximumCapacity = maximunCapacity;
+		this.maximumCapacity = maximumCapacity;
 	}
 
 	public List<Contact> getContacts() {
@@ -35,6 +35,62 @@ public class Agenda {
 			return "The agenda is full.";
 		contacts.add(contact);
 		return "Contact added successfully.";
+	}
+
+	public void thereIsContact(Contact contact) {
+		if (contacts.contains(contact))
+			System.out.println("The contact name already exists.");
+		else {
+			contacts.add(contact);
+			System.out.println("Enter the contact's name.");
+		}
+	}
+
+	public void listContact() {
+		boolean emptyAgenda = true;
+		System.out.println("Contact list");
+		for (Contact contact : contacts) {
+			if (contact != null) {
+				System.out.println(contact.toString());
+				emptyAgenda = false;
+			}
+		}
+		if (emptyAgenda) {
+			System.out.println("The agenda is empty");
+		}
+	}
+
+	public Contact searchContact(Contact contact) {
+		for (Contact contactList : contacts) {
+			if (contactList.getName().equals(contact.getName()) && contactList.getLast_name().equals(contact.getLast_name())) {
+				return contactList;
+			}
+		}
+		return null;
+	}
+
+	public void deleteContact(String name, String last_name) {
+		for (int i = 0; i < contacts.size(); i++) {
+			Contact contact = contacts.get(i);
+			if (contact.getName().equals(name) && contact.getLast_name().equals(last_name)) {
+				contacts.remove(i);
+				System.out.println("Contact removed successfully.");
+			}
+		}
+	}
+
+	public String modifyContact(Contact contact) {
+		Contact contactHome = searchContact(Contact contact);
+		if (contactHome != null) {
+			contact.setNumber(newNumber);
+			System.out.println("Number of" + name + "modified by " + newNumber + "successfully.");
+		} else {
+			System.out.println("No se puede modificar. El contacto " + name + "no exist in the agenda.");
+		}
+	}
+
+	public String freeSpaces() {
+		int busy
 	}
 
 }
